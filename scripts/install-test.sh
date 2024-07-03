ebe7d1c8aa095dda0a3c2aa2fcb19af18f75eaf5
Katherine Wu <kwu@stripe.com>
2024-07-03 11:12:05 -0700

    try fix

--- scripts/install-test.sh
+++ scripts/install-test.sh
@@ -21,7 +21,6 @@ run_install() {
     ;;
 
     yum)
-        echo "[Stripe]\nname=stripe\nbaseurl=https://packages.stripe.dev/stripe-cli-rpm-local/\nenabled=1\ngpgcheck=0" >> /etc/yum.repos.d/stripe.repo
         yum -y install stripe
     ;;
 
